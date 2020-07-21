f06ede56ee85fed84f19b141179796dcc500d566
Jason Kim <iamjsonkim@gmail.com>
2020-07-20 22:22:29 -0700

    Added Google analytics into html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
-import ReactGA from 'react-ga';
 
-ReactGA.initialize('UA-28018879-8');
 render(<App />, document.getElementById('app'));
